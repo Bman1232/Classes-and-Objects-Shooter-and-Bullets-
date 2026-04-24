cc8a7bdfda7493a571ed8b3ac865a73cec3b3734
Burke Murrill <burke.murrill@gmail.com>
2026-04-24 16:34:32 -0500

    comit2

--- main.py
+++ main.py
@@ -18,11 +18,12 @@ def playing_area():
     pen.end_fill()
     
 class Player(Turtle):
-    def __init__(self, x, y, color, screen, right_key, left_key, fire_key):
+    def __init__(self, x, y, color, screen, right_key, left_key, fire_key, health, outline_color):
         super().__init__()
         self.ht()
         self.speed(0)
         self.player_color = color
+        self.pencolor(outline_color)
         self.color(color)
         self.penup()
         self.goto(x,y)
@@ -31,6 +32,7 @@ class Player(Turtle):
         self.bullets = []
         self.alive = True
         self.st()
+        self.health = 3
         screen.onkeypress(self.turn_left, left_key)
         screen.onkeypress(self.turn_right, right_key)
         screen.onkey(self.fire, fire_key)
@@ -50,6 +52,15 @@ class Player(Turtle):
     
     def fire(self):
         self.bullets.append(Bullet(self))
+    
+    def damage(self, outline_color):
+        self.health -= 1
+        if self.health == 2:
+            self.color("yellow")
+            self.pencolor(outline_color)
+        elif self.health == 1:
+            self.color("red")
+            self.pencolor(outline_color)
 
 class Bullet(Turtle):
     def __init__(self, player):
@@ -67,12 +78,15 @@ class Bullet(Turtle):
     def move(self, player):
         self.forward(10)
         if self.xcor() > 230 or self.xcor() < -230:
-            self.ht()
-            player.bullets.remove(self)
+            self.kill()
         if self.ycor() > 230 or self.ycor() < -230:
+            self.kill()
+
+    def kill(self):
+        if self in self.player.bullets:
             self.ht()
-            player.bullets.remove(self)
-    
+            self.player.bullets.remove(self)
+
     def die(self):
         pass
 screen = Screen()
@@ -84,16 +98,33 @@ screen.listen()
 
 playing_area()
 
-p1 = Player(-100, 0, "red",screen, "d", "a", "w")
-p2 = Player(100,0,"blue",screen, "Right","Left", "Up")
+p1 = Player(-100, 0, "red",screen, "d", "a", "w", 3, "red")
+p2 = Player(100,0,"blue",screen, "Right","Left", "Up", 3, "blue")
 
 while p1.alive and p2.alive:
     p1.move()
     p2.move()
     for bullet in p1.bullets:
         bullet.move(p1)
+        if p2.distance(bullet) < 20:
+            bullet.kill()
+            p2.damage("blue")
+            print("Damage taken")
+
     for bullet in p2.bullets:
         bullet.move(p2)
+        if p1.distance(bullet) < 20:
+            bullet.kill()
+            p1.damage("red")
+            print("Damage taken")
+    
+    if p1.health < 1:
+        p1.hideturtle()
+        p1.remove()
+    if p2.health < 1:
+        p2.hideturtle()
+        p2.remove()
+
 
 
 
